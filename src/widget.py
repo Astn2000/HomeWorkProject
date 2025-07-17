6be7b70e34d8c21a0047cab8ca917ab5f59960bd
NikitaAstanin <nikita139662@gmail.com>
2025-07-17 12:36:11 +0300

    fixed3

--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,13 @@
-import masks # type: ignore
+from masks import get_mask_account, get_mask_card_number # type: ignore
 
 
-def mask_account_card(card_type: str, card_number: str) -> str:
+def mask_account_card(card_type: str, account_or_card_number: str) -> str:
     """Функция принимает тип счета или карты и его номер , выводит зашифрованный номер"""
-    if card_type != "Счет":
-        return f"{card_type} {card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-    return f"{card_type} **{card_number[-4:]}"
+    if card_type == "Счет":
+        account_number_1 = get_mask_account(account_or_card_number)
+        return f"{card_type} {account_number_1}"
+    card_number_1 = get_mask_card_number(account_or_card_number)
+    return f"{card_type} {get_mask_card_number(card_number_1)}"
 
 
 def get_date(string_1: str) -> str:
@@ -16,8 +18,6 @@ def get_date(string_1: str) -> str:
 
 
 if __name__ == "__main__":
-    print(mask_account_card("Visa Gold", "5999414228426353"))
-    print(mask_account_card("Счет", "64686473678894779589"))
+    print(mask_account_card("Счет", "73654108430135874305"))
+    print(mask_account_card("Visa Gold", "1234567812345678"))
     print(get_date("2024-03-11T02:26:18.671407"))
-    print(masks.get_mask_card_number(1234567812345678))
-    print(masks.get_mask_account(73654108430135874305))

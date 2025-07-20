7f187f972cf1c201ede958db3eea9d2c06a3977b
NikitaAstanin <nikita139662@gmail.com>
2025-07-20 16:21:44 +0300

    add new function

---
+++ src/processing.py
@@ -0,0 +1,28 @@
+
+
+
+def filter_by_state(my_list: list, state='EXECUTED') -> list:
+    new_list = list()
+    for list_ in my_list:
+        status = list_.get('state')
+        if state == status:
+            new_list.append(list_)
+    return new_list
+
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
+
+
+
+def sort_by_date(list_dict: list, optional_parameter=False):
+    sorted_list = sorted(list_dict, key=lambda date: date['date'], reverse=optional_parameter)
+    return sorted_list
+
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True))

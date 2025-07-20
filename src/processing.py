4fd61df107b0fdcb04b1cd1481bd66b667312627
NikitaAstanin <nikita139662@gmail.com>
2025-07-20 16:37:12 +0300

    fixes wight lint

--- src/processing.py
+++ src/processing.py
@@ -1,28 +1,37 @@
-
-
-
-def filter_by_state(my_list: list, state='EXECUTED') -> list:
+def filter_by_state(my_list: list, state: str = "EXECUTED") -> list:
     new_list = list()
     for list_ in my_list:
-        status = list_.get('state')
+        status = list_.get("state")
         if state == status:
             new_list.append(list_)
     return new_list
 
 
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        "CANCELED",
+    )
+)
 
 
-
-def sort_by_date(list_dict: list, optional_parameter=True):
-    sorted_list = sorted(list_dict, key=lambda date: date['date'], reverse=optional_parameter)
+def sort_by_date(list_dict: list, optional_parameter: bool =True) -> list:
+    sorted_list = sorted(list_dict, key=lambda date: date["date"], reverse=optional_parameter)
     return sorted_list
 
 
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)

91defe5e33750cd117cf173dd7ff3dd215899592
NikitaAstanin <nikita139662@gmail.com>
2025-07-20 16:30:25 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -17,7 +17,7 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
 
 
 
-def sort_by_date(list_dict: list, optional_parameter=False):
+def sort_by_date(list_dict: list, optional_parameter=True):
     sorted_list = sorted(list_dict, key=lambda date: date['date'], reverse=optional_parameter)
     return sorted_list
 
@@ -25,4 +25,4 @@ def sort_by_date(list_dict: list, optional_parameter=False):
 print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True))
+              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
